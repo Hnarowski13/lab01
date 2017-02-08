bf718b7ba88a4dbb8ea189581a9bb47cf9a221eb
Helena Narowski <narowsk4@cse.msu.edu>
2017-02-07 20:57:48 -0500

    erased my hard word

--- lab1Client.cc
+++ lab1Client.cc
@@ -28,49 +28,6 @@ int main(int argc, char *argv[]) {
   std::cout << "[TCP] Connecting to server: " << serverNameStr
             << ":" << tcpServerPort << std::endl;
     
-    struct hostent *hostEnd = gethostbyname(serverNameStr);
-    
-//    if (lh)
-//        puts(lh->h_name);
-//    else
-//        herror("gethostbyname");
-    while(cin.eof() == false) {
-        // create a TCP socket with protocol family AF_INET
-        tcpConnectionFd = socket(AF_INET, SOCK_STREAM, 0);
-        
-        // if the return value is -1, the creation of socket is failed.
-        if(tcpConnectionFd < 0) {
-            cerr << "[ERR] Unable to create TCP socket." << endl;
-            exit(1);
-        }
-        
-        // initialize the socket address strcut by setting all bytes to 0
-        memset(&tcpServerAddr, 0, sizeof(tcpServerAddr));
-        
-        // details are covered in class/slides
-        tcpServerAddr.sin_family = AF_INET;
-        tcpServerAddr.sin_port   = htons(tcpServerPort);
-        
-         MyPacket outgoingPkt;
-        memset(&outgoingPkt, 0, sizeof(outgoingPkt));
-     //  char typeName[typeNameLen];
-      //  memset(&typeName, 0, sizeof(typeName));
-        
-        outgoingPkt.type = JOIN;
-        
-        memcpy(&tcpServerAddr.sin_addr, hostEnd -> h_addr,
-               hostEnd -> h_length);
-        
-        if(connect(tcpConnectionFd, (sockaddr *)&tcpServerAddr,
-                   sizeof(tcpServerAddr)) < 0) {
-            cerr << "[ERR] Unable to connect to server." << endl;
-            if(close(tcpConnectionFd) < 0) {
-                cerr << "[ERR] Error when closing TCP socket" << endl;
-            }
-            exit(1);
-        }
-    }
-    
     return 0;
   // Sample use of TicTacToe
 //  TicTacToe game;
